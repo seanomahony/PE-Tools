293886b623cdb629fd87b3ede85263c9810521dc
Andy <andy.tomlinson@practiceevolve.com>
2022-11-23 13:13:51 +0000

    added new list item class

---
+++ PE-Tools/Models/DatabaseListItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE_Tools.Models
+{
+    public class DatabaseListItem
+    {
+        public static int CurrentIndex = 0;
+        public int ID { get; set; }
+        public string Name { get; set; }
+
+        public DatabaseListItem(string name)
+        {
+            ID = CurrentIndex++;
+            Name = name;
+        }
+    }
+}
