44d8a3d3b2d9c0a918a62d0e983177b29daab160
Andy <andy.tomlinson@practiceevolve.com>
2022-11-26 13:27:54 +0000

    Add admin permissions to run PE scripts (if atarted in Admin mode)

--- PE-Tools/Views/PowershellCommandsView.Designer.cs
+++ PE-Tools/Views/PowershellCommandsView.Designer.cs
@@ -32,19 +32,21 @@
             this.btnRunCommand = new System.Windows.Forms.Button();
             this.lblCommand = new DevExpress.XtraEditors.LabelControl();
             this.tbResults = new System.Windows.Forms.TextBox();
+            this.btnBuild = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // tbCommand
             // 
             this.tbCommand.Location = new System.Drawing.Point(82, 24);
             this.tbCommand.Name = "tbCommand";
-            this.tbCommand.Size = new System.Drawing.Size(560, 20);
+            this.tbCommand.Size = new System.Drawing.Size(354, 20);
             this.tbCommand.TabIndex = 0;
             this.tbCommand.KeyDown += new System.Windows.Forms.KeyEventHandler(this.tbCommand_KeyDown);
             // 
             // btnRunCommand
             // 
-            this.btnRunCommand.Location = new System.Drawing.Point(648, 22);
+            this.btnRunCommand.Location = new System.Drawing.Point(547, 22);
             this.btnRunCommand.Name = "btnRunCommand";
             this.btnRunCommand.Size = new System.Drawing.Size(75, 23);
             this.btnRunCommand.TabIndex = 1;
@@ -71,16 +73,39 @@
             this.tbResults.Size = new System.Drawing.Size(705, 313);
             this.tbResults.TabIndex = 3;
             // 
+            // btnBuild
+            // 
+            this.btnBuild.Location = new System.Drawing.Point(454, 22);
+            this.btnBuild.Name = "btnBuild";
+            this.btnBuild.Size = new System.Drawing.Size(75, 23);
+            this.btnBuild.TabIndex = 4;
+            this.btnBuild.Text = "Build";
+            this.btnBuild.UseVisualStyleBackColor = true;
+            this.btnBuild.Click += new System.EventHandler(this.btnBuild_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(648, 22);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 5;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
             // PowershellCommandsView
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnBuild);
             this.Controls.Add(this.tbResults);
             this.Controls.Add(this.lblCommand);
             this.Controls.Add(this.btnRunCommand);
             this.Controls.Add(this.tbCommand);
             this.Name = "PowershellCommandsView";
             this.Size = new System.Drawing.Size(744, 379);
+            this.Load += new System.EventHandler(this.PowershellCommandsView_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -92,5 +117,7 @@
         private System.Windows.Forms.Button btnRunCommand;
         private DevExpress.XtraEditors.LabelControl lblCommand;
         private System.Windows.Forms.TextBox tbResults;
+        private System.Windows.Forms.Button btnBuild;
+        private System.Windows.Forms.Button btnStop;
     }
 }
--- PE-Tools/Views/PowershellCommandsView.cs
+++ PE-Tools/Views/PowershellCommandsView.cs
@@ -5,9 +5,19 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Management.Automation.Runspaces;
+using System.Security.Principal;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+
+
+//In Package Manager Console change dir to get to project folder then add the following package for WinForms
+//cd  PE-Tools
+//dotnet add package Microsoft.PowerShell.5.ReferenceAssemblies --version 1.1.0
+
+//For non-historic apps just install System.Management.Automation using Nuget
+
+//these both give you System.Management.Automation.Runspaces
 
 namespace PE_Tools.Views
 {
@@ -18,7 +28,12 @@ namespace PE_Tools.Views
             InitializeComponent();
         }
 
-        private string RunScript(string script)
+        private void PowershellCommandsView_Load(object sender, EventArgs e)
+        {
+            checkElevatedMode();
+        }
+
+        private string RunScript(string script, bool requiresAuth = false, string changeDir = null)
         {
             var sb = new StringBuilder();
             try
@@ -26,6 +41,14 @@ namespace PE_Tools.Views
                 var runspace = RunspaceFactory.CreateRunspace();
                 runspace.Open();
                 var pipeline = runspace.CreatePipeline();
+                if (requiresAuth)
+                {
+                    pipeline.Commands.AddScript("Set-ExecutionPolicy Unrestricted");
+                }
+                if(!string.IsNullOrEmpty(changeDir))
+                {
+                    pipeline.Commands.AddScript($"cd {changeDir}");
+                }
                 pipeline.Commands.AddScript(script);
                 pipeline.Commands.Add("Out-String");
                 var results = pipeline.Invoke();
@@ -44,10 +67,19 @@ namespace PE_Tools.Views
             return sb.ToString();
         }
 
-        private void btnRunCommand_Click(object sender, EventArgs e)
+        private void ShowMessage(string msg)
         {
             tbResults.Clear();
-            tbResults.Text = RunScript(tbCommand.Text);
+            tbResults.Text = RunScript(msg);
+        }
+
+        private void checkElevatedMode()
+        {
+            var principal = new WindowsPrincipal(WindowsIdentity.GetCurrent());
+            if(!principal.IsInRole(WindowsBuiltInRole.Administrator))
+            {
+                ShowMessage("echo \'You need to run this application as administrator to use most powershell tools\'");
+            }
         }
 
         private void tbCommand_KeyDown(object sender, KeyEventArgs e)
@@ -57,6 +89,22 @@ namespace PE_Tools.Views
                 tbResults.Clear();
                 tbResults.Text = RunScript(tbCommand.Text);
             }
+        }
+        private void btnRunCommand_Click(object sender, EventArgs e)
+        {
+            tbResults.Clear();
+            tbResults.Text = RunScript("./RunPE.ps1", true);
+        }
+        private void btnBuild_Click(object sender, EventArgs e)
+        {
+            tbResults.Clear();
+            tbResults.Text = RunScript("./start.ps1 -build", true,@"c:\dev\onprem\");
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            tbResults.Clear();
+            tbResults.Text = RunScript("./StopPE.ps1", true);
         }
     }
 }
