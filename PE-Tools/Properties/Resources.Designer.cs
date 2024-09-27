6d3d65224b2dd3ef7884ebde11ad5585d64c2e57
seanomahony <sean.omahony@practiceevolve.com>
2024-09-27 16:06:57 +0100

    Remove background image

--- PE-Tools/Properties/Resources.Designer.cs
+++ PE-Tools/Properties/Resources.Designer.cs
@@ -59,15 +59,5 @@ namespace PE_Tools.Properties {
                 resourceCulture = value;
             }
         }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap blur {
-            get {
-                object obj = ResourceManager.GetObject("blur", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
     }
 }
