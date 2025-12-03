369e0c765fca683d2e4b5289729e7e69aa640492
seanomahony <sean.omahony@practiceevolve.com>
2025-12-03 13:36:02 +0000

    Commit changes before fixing errors.

--- PE-Tools/Form1.cs
+++ PE-Tools/Form1.cs
@@ -6,7 +6,6 @@ using System.Configuration;
 using System.Data;
 using System.Drawing;
 using System.Linq;
-using System.Management.Automation;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
--- PE-Tools/Views/PowershellCommandsView.cs
+++ PE-Tools/Views/PowershellCommandsView.cs
@@ -4,7 +4,6 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
-using System.Management.Automation.Runspaces;
 using System.Security.Principal;
 using System.Text;
 using System.Threading.Tasks;
